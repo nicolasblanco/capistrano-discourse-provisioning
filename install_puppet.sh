4b3d036882874e82f755699bc687f335ac64e443
slainer68 <slainer68@gmail.com>
2013-12-18 22:50:43 +0100

    install script in cap recipe

--- install_puppet.sh
+++
@@ -1,7 +0,0 @@
-wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb
-sudo dpkg -i puppetlabs-release-precise.deb
-sudo apt-get update
-sudo apt-get install -y puppet-common
-
-cp /etc/puppet/manifests/sample_config.pp /etc/puppet/manifests/config.pp
-echo "Please update config.pp with the required variables"
